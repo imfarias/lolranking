63f46f6b027be9ce49c046adf02eed174d412c2b
root <root@DESKTOP-FDAQQRO.localdomain>
2020-10-28 23:55:04 -0300

    Flex update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const fetch = require('node-fetch');
 const http = require('http');
+const url = require('url');
 
 http.createServer(onRequest).listen(process.env.PORT || 8080);
 
 function onRequest(request, response) {
 
     const finalURL = "https://app.mobalytics.gg/api/lol/summoners/v1/BR/@SUBSTITUIR_USUARIO@/profile";
+
+    const url_parts = url.parse(request.url, true);
+    const query = url_parts.query;
+
+    let type = query.queue ? query.queue : 'RANKED_SOLO_DUO';
 
     const tierRanking = [
         'CHALLENGER',
@@ -46,84 +52,89 @@ function onRequest(request, response) {
 
             if (data.data.gameTypes && data.data.gameTypes.length) {
                 let gameType = data.data.gameTypes.filter(gameType => {
-                    return gameType.name === 'RANKED_SOLO_DUO';
+                    return gameType.name === type;
                 }).shift();
 
-                ranking.push({
-                    user: user,
-                    icon: data.data.profile.summoner.icon,
-                    tier: gameType.rank.tier,
-                    division: gameType.rank.division,
-                    lp: gameType.lp,
-                    wins: gameType.wins,
-                    losses: gameType.losses
-                });
+                if(gameType) {
 
-                ranking.sort(function (a, b) {
-                    if (tierRanking.indexOf(a.tier) > tierRanking.indexOf(b.tier)) {
-                        return 1;
-                    } else if (tierRanking.indexOf(a.tier) < tierRanking.indexOf(b.tier)) {
-                        return -1;
-                    } else {
-                        if (a.division > b.division) {
+                    ranking.push({
+                        user: user,
+                        icon: data.data.profile.summoner.icon,
+                        tier: gameType.rank.tier,
+                        division: gameType.rank.division,
+                        lp: gameType.lp,
+                        wins: gameType.wins,
+                        losses: gameType.losses
+                    });
+
+                    ranking.sort(function (a, b) {
+                        if (tierRanking.indexOf(a.tier) > tierRanking.indexOf(b.tier)) {
                             return 1;
-                        } else if (a.division < b.division) {
+                        } else if (tierRanking.indexOf(a.tier) < tierRanking.indexOf(b.tier)) {
                             return -1;
                         } else {
-                            if (a.lp == b.lp) {
-                                let aWins = a.wins / a.losses;
-                                let bWins = b.wins / b.losses;
+                            if (a.division > b.division) {
+                                return 1;
+                            } else if (a.division < b.division) {
+                                return -1;
+                            } else {
+                                if (a.lp == b.lp) {
+                                    let aWins = a.wins / a.losses;
+                                    let bWins = b.wins / b.losses;
 
-                                if (aWins > bWins) {
-                                    return -1;
-                                } else if (aWins < bWins) {
-                                    return 1;
+                                    if (aWins > bWins) {
+                                        return -1;
+                                    } else if (aWins < bWins) {
+                                        return 1;
+                                    }
+
+                                    return 0;
                                 }
-
-                                return 0;
+                                return a.lp < b.lp ? 1 : -1;
                             }
-                            return a.lp < b.lp ? 1 : -1;
                         }
+                    });
+
+                    if(ranking.length === userList.length) {
+
+                        let html = `<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+                        <table class="table table-striped table-dark">
+                            <thead>
+                                <tr>
+                                    <td>#</td>
+                                    <td>Ícone</td>
+                                    <td>Nome</td>
+                                    <td>Elo</td>
+                                    <td>PDL</td>
+                                    <td>Vitórias</td>
+                                    <td>Derrotas</td>
+                                </tr>
+                            </thead>
+                            <tbody>`;
+
+                        ranking.forEach((user, index) => {
+                            html += `<tr>
+                                <td>${index + 1}</td>
+                                <td><img src="https://cdn.mobalytics.gg/assets/lol/images/summoner-icon/${user.icon}.png" width="50px" heigth="50px" style="border-radius=50%"></td>
+                                <td>${user.user}</td>
+                                <td>${user.tier} ${user.division}</td>
+                                <td>${user.lp}</td>
+                                <td>${user.wins}</td>
+                                <td>${user.losses}</td>
+                            </tr>`;
+                        });
+
+                        html += `</tbody>
+                        </table>`;
+
+                        response.writeHead(200, {
+                            'Content-Type': 'text/html; charset=utf-8'
+                        });
+                        response.write(html);
+                        response.end();
                     }
-                });
-
-                if(ranking.length === userList.length) {
-
-                    let html = `<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-                    <table class="table table-striped table-dark">
-                        <thead>
-                            <tr>
-                                <td>#</td>
-                                <td>Ícone</td>
-                                <td>Nome</td>
-                                <td>Elo</td>
-                                <td>PDL</td>
-                                <td>Vitórias</td>
-                                <td>Derrotas</td>
-                            </tr>
-                        </thead>
-                        <tbody>`;
-
-                    ranking.forEach((user, index) => {
-                        html += `<tr>
-                            <td>${index + 1}</td>
-                            <td><img src="https://cdn.mobalytics.gg/assets/lol/images/summoner-icon/${user.icon}.png" width="50px" heigth="50px" style="border-radius=50%"></td>
-                            <td>${user.user}</td>
-                            <td>${user.tier} ${user.division}</td>
-                            <td>${user.lp}</td>
-                            <td>${user.wins}</td>
-                            <td>${user.losses}</td>
-                        </tr>`;
-                    });
-
-                    html += `</tbody>
-                    </table>`;
-
-                    response.writeHead(200, {
-                        'Content-Type': 'text/html; charset=utf-8'
-                    });
-                    response.write(html);
-                    response.end();
+                } else {
+                    userList.splice(userList.indexOf(user), 1);
                 }
 
             }
